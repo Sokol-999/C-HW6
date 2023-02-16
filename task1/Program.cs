0a1416990c13cc21a0113b7b9f24462788d18122
Sokolov Ilya <sokolov.ilia.2000@mail.ru>
2023-02-16 18:56:39 +0300

    HW6

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0
+//  ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+System.Console.WriteLine("Write numbers with 'space': ");
+// int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+var arr = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) count++;
+}
+System.Console.WriteLine(count);
---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+//   уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
+//    задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+int InputNumber(string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("ERROR, try again");
+    }
+    return number;
+}
+
+double b1 = InputNumber("Write number b1: ");
+double k1 = InputNumber("Write number k1: ");
+double b2 = InputNumber("Write number b2: ");
+double k2 = InputNumber("Write number k2: ");
+
+
+// (k1 * x) + b1 == (k2 * x) + b2;
+// x(k1-k2)==b2-b1;
+// x==(b2-b1)/(k1-k2);
+// y=(k1*x)+b1
+
+double x = (b2 - b1) / (k1 - k2);
+double y = (k1 * x) + b1;
+System.Console.WriteLine();
+System.Console.WriteLine($"DOT: {(x, y)}");
